769ca190050a364a71b51e31ebcae1467c9b5f2f
João <joao.favareto@aluno.senai.br>
2025-06-18 12:15:30 -0300

    incluindo nota3

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ function calcularMedia() {
     // pega os valores dos campos
     var n1 = parseFloat(document.getElementById('nota1').value);
     var n2 = parseFloat(document.getElementById('nota2').value);
+    var n3 = parseFloat(document.getElementById('nota3').value);
  
          // verifica se são números validos
-    if (isNaN(n1) || isNaN(n2)){
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3)){
         alert("Por Favor, insira notas válidas");
         return;
     }
  
     // calcula a média//
-    var media= (n1 + n2) / 2;
+    var media= (n1 + n2 + n3) / 3;
  
     //mostra o resultado//
     document.getElementById('resultado').innerText= "A média é: " + media.toFixed(2);
